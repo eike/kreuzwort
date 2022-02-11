afeb488d365c93b8de179809113c17d4f9ceca1a
Eike Schulte <eike@kifu.eu>
2022-02-11 17:20:56 +0100

    Directional mouse cursor for cells

--- src/Cell.ts
+++ src/Cell.ts
@@ -1,7 +1,16 @@
+enum PositionInCell {
+    Center,
+    Top,
+    Right,
+    Bottom,
+    Left,
+}
+
 class Cell extends HTMLElement {
     shadow : ShadowRoot;
     entry : HTMLSpanElement;
     background : HTMLDivElement;
+    cursorDiv: HTMLDivElement;
 
     constructor() {
         super();
@@ -10,8 +19,24 @@ class Cell extends HTMLElement {
 
         let style = document.createElement('style');
         style.textContent = `
-        :root { background-color: green; }
-        div { width: var(--cell-width); height: var(--cell-height); z-index: -2; position: absolute; top: 0; left: 0; box-sizing: border-box; }
+        div {
+            width: var(--cell-width); 
+            height: var(--cell-height); 
+            z-index: -2; 
+            position: absolute; 
+            top: 0; left: 0; 
+            box-sizing: border-box;
+            cursor: cell;
+        }
+        div.cursor {
+            z-index: 1;
+        }
+        div.cursor.across {
+            cursor: text;
+        }
+        div.cursor.down {
+            cursor: vertical-text;
+        }
         div.current-word {
             background-color: var(--current-light);
         }
@@ -19,15 +44,36 @@ class Cell extends HTMLElement {
             border-left: 3px solid var(--cursor-color);
         }
         div.after {
-            border-right: 0px solid var(--cursor-color);
+            box-shadow: 3px 0 0 0 var(--cursor-color);
         }
         `;
         this.shadow.appendChild(style);
         this.entry = document.createElement('span');
         this.shadow.appendChild(this.entry);
         
+        this.cursorDiv = document.createElement('div');
+        this.cursorDiv.classList.add('cursor');
+        this.shadow.appendChild(this.cursorDiv);
+
         this.background = document.createElement('div');
         this.shadow.appendChild(this.background);
+
+        this.addEventListener('mousemove', (e) => {
+            this.cursorDiv.classList.remove('across');
+            this.cursorDiv.classList.remove('down');
+            switch (this.positionInCell(e.offsetX, e.offsetY)) {
+                case PositionInCell.Center:
+                    break;
+                case PositionInCell.Left:
+                case PositionInCell.Right:
+                    this.cursorDiv.classList.add('across');
+                    break;
+                case PositionInCell.Top:
+                case PositionInCell.Bottom:
+                    this.cursorDiv.classList.add('down');
+                    break;
+            }
+        });
     }
 
     connectedCallback(): void {
@@ -38,20 +84,35 @@ class Cell extends HTMLElement {
         });
 
         crossword?.cells.get(this)?.on('highlightChanged', (highlight) => {
+            this.cursorDiv.classList.remove('before');
+            this.cursorDiv.classList.remove('after');
+            this.background.classList.remove('current-word');
             switch (highlight) {
                 case CellHighlight.None:
-                    this.background.className = "";
                     break;
                 case CellHighlight.CurrentWord:
-                    this.background.className = "current-word";
+                    this.background.classList.add('current-word');
                     break;
                 case CellHighlight.CursorAfter:
-                    this.background.className = "current-word after";
+                    this.background.classList.add('current-word');
+                    this.cursorDiv.classList.add('after');
                     break;
                 case CellHighlight.CursorBefore:
-                    this.background.className = "current-word before";
+                    this.background.classList.add('current-word');
+                    this.cursorDiv.classList.add('before');
                     break;
             }
         });
+    }
+
+    positionInCell(x : number, y : number) : PositionInCell {
+        let closestEdge = [ 
+                [ x, PositionInCell.Left ], 
+                [ this.offsetWidth - x, PositionInCell.Right ],
+                [ y, PositionInCell.Top ],
+                [ this.offsetHeight - y, PositionInCell.Bottom ],
+            ].sort((a, b) => a[0] - b[0])[0];
+        if (closestEdge[0] <= 10) return closestEdge[1];
+        else return PositionInCell.Center;
     }
 }
